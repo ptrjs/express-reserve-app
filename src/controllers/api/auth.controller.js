582e2b396cbfe070bec944a498febc9dcba4111b
ptrjs <peterjose64@gmail.com>
2024-07-20 20:17:44 +0700

    first commit

--- src/controllers/api/auth.controller.js
+++ src/controllers/api/auth.controller.js
@@ -1,7 +1,7 @@
 const httpStatus = require('http-status');
-const catchAsync = require('../utils/catchAsync');
-const { authService, userService, tokenService, emailService } = require('../services');
-const ApiError = require('../utils/ApiError');
+const catchAsync = require('../../utils/catchAsync');
+const { authService, userService, tokenService, emailService } = require('../../services');
+const ApiError = require('../../utils/ApiError');
 
 const register = catchAsync(async (req, res) => {
   const existingUser = await userService.getUserByEmail(req.body.email);
--- src/controllers/api/user.controller.js
+++ src/controllers/api/user.controller.js
@@ -1,8 +1,8 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
-const ApiError = require('../utils/ApiError');
-const catchAsync = require('../utils/catchAsync');
-const { userService } = require('../services');
+const pick = require('../../utils/pick');
+const ApiError = require('../../utils/ApiError');
+const catchAsync = require('../../utils/catchAsync');
+const { userService } = require('../../services');
 
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
@@ -16,7 +16,7 @@ const createUser = catchAsync(async (req, res) => {
 
 const getUsers = catchAsync(async (req, res) => {
   const result = await userService.queryUsers();
-  
+
   res.status(httpStatus.OK).send({
     status: httpStatus.OK,
     message: "Get Users Success",
@@ -29,7 +29,7 @@ const getUser = catchAsync(async (req, res) => {
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
   }
-  
+
   res.status(httpStatus.OK).send({
     status: httpStatus.OK,
     message: "Get User Success",
@@ -39,7 +39,7 @@ const getUser = catchAsync(async (req, res) => {
 
 const updateUser = catchAsync(async (req, res) => {
   const user = await userService.updateUserById(req.params.userId, req.body);
-  
+
   res.status(httpStatus.OK).send({
     status: httpStatus.OK,
     message: "Update User Success",
@@ -49,7 +49,7 @@ const updateUser = catchAsync(async (req, res) => {
 
 const deleteUser = catchAsync(async (req, res) => {
   await userService.deleteUserById(req.params.userId);
-  
+
   res.status(httpStatus.OK).send({
     status: httpStatus.OK,
     message: "Delete User Success",
